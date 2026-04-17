c35cd252c12cd9e4116ea1e95eb92a6c8f414ec5
harshpatel2405 <harshpatel4504@gmail.com>
2026-04-17 17:57:57 +0530

    classes and objects_1

---
+++ oops/1_co.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self):
+        self.a = 45
+        self.b = 98
+
+    def display(self):
+        print("A :" , self.a)
+        print("B :" , self.b)
+
+s1 = Student() 
+s2 =Student()  
+s1.display()     
